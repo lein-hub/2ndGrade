8e04db6301bcbd243fef3e92e21e652b480ca526
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-05-17 21:00:01 +0900

    coding with scpark

--- src/codingwithscpark/SimpleDictionary.java
+++ src/codingwithscpark/SimpleDictionary.java
@@ -29,7 +29,7 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 	private Properties props;
 	
 	private static final String JDBC_CLASS_NAME = "org.mariadb.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mariadb://localhost:3306/oop3";
+	private static final String DB_URL = "jdbc:mariadb://127.0.0.1:3306/oop3";
 	
 	// 한영사전 : 한글단어와 대응되는 영어단어의 쌍을 저장
 	private Map<String, String> dict = new HashMap<>();
@@ -70,7 +70,7 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 //				rs.getString(1);
 				String key = rs.getString("kor");
 				String value = rs.getString("eng");
-				
+				dict.put(key, value);
 				System.out.println(key + ":" + value);
 			}
 		} catch (SQLException e) {
@@ -82,7 +82,42 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 	}
 	
 	private void addWordToDB(String key, String value) {
-		
+		/*
+		 * 드라이버를 메모리에 적재한다 <- 이미 생성자에서 했다.
+		 * DB에 연결해서 Connection 객체를 반환받는다.
+		 * Connection 객체에게 PreparedStatement 객체를 요청한다.
+		 * PreparedStatement 객체의 executeUpdate() 메서드를 호출해서 DB에 저장한다.
+		 */
+		try(Connection con = DriverManager.getConnection(DB_URL, "root", "")) {
+			String sql = "insert into dict values(?, ?)";
+			/*
+			 * 실행 준비
+			 * 1. 문법 검사 (sql변수의 문법을 검사)
+			 * 2. 정당성 검사(테이블, 칼럼 등이 실제로 있는지, 있다면 이 사용자가 레코드를 삽입할 권한이 있는지)
+			 * 3. 실행 계획을 세운다. (execution plan)
+			 */
+			PreparedStatement pst = con.prepareStatement(sql);
+			// ? 자리의 칼럽 데이터 타입에 따라 적절한 set~ 메소드를 호출해야한다
+			// 예를 들어, 칼럼이 char 또는 varchar타입이면 setString()
+			// 칼럼이 TimeStamp 타입이면 setData(), setTimestamp()
+			// 칼럼이 int 타입이면 setInt()
+			pst.setString(1, key);
+			pst.setString(2, value);
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void addWorldToFile(String key, String value) {
+		try(FileWriter writer = new FileWriter(DIC_FILE_NAME, true)) {
+			String str = key + "=" + value + "\n";
+			writer.write(str);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	private void buildDictionaryFromFile() {
@@ -136,6 +171,7 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 			String eng = JOptionPane.showInputDialog(this, key + "에 대응하는 영어단어를 입력하세요");
 			if (eng == null || eng.trim().length()==0) return;
 			dict.put(key, eng);
+			dict.put(eng, key);
 //			try (PrintWriter out = new PrintWriter(new File(DIC_FILE_NAME))) {
 //				props.put(key, eng);
 //				props.store(out, "My Dictionary");
@@ -143,6 +179,7 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 //				System.out.println(e1.getMessage());
 //			}
 			
+			// addWordToFile
 			try(FileWriter writer = new FileWriter(DIC_FILE_NAME, true)) {
 				String str = key + "=" + eng + "\n";
 				writer.write(str);
@@ -150,8 +187,13 @@ public class SimpleDictionary extends JPanel implements ActionListener{
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+			
+			// addWordToDB
+			addWordToDB(key, eng);
 			JOptionPane.showMessageDialog(this, "영어단어가 추가되었습니다");
 		}
 		inputField.requestFocus();
 	}
+	
+	
 }
